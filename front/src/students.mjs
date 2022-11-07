052f693068de0e6fbed4fcbf5374c68e5eb4089f
John Alexis Guerra Gomez <john.guerra@gmail.com>
2022-11-07 10:47:34 -0800

    Adding fake students.mjs

---
+++ front/src/students.mjs
@@ -0,0 +1,16 @@
+export const classes = {
+  class1: [
+    "Student 1",
+    "Student 2",
+    "Student 3",
+    "Student 4",
+    "Student 5",
+    "Student 6",
+    "Student 7",
+    "Student 8",
+    "Student 9",
+    "Student 10",
+    "Student 11",
+    "Student 12",
+  ],
+};
